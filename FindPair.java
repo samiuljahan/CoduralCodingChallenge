994ad05efe01f1fe8518a043ac20d6e1b4271d93
Samiul Jahan <samiuljahan@gmail.com>
2020-04-01 02:50:06 -0500

    Solution to FindPair problem

---
+++ FindPair.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Main {
+  public static void main(String[] args) {
+    int[] a = {1, 4, 2, 6, 10, 8};
+    System.out.println(Arrays.toString(findPair(a, 10)));
+  }
+  static int[] findPair(int[] a, int key){
+    Map<Integer, Integer> map = new HashMap<>();
+    for(int i = 0; i < a.length; i++)
+    {
+      if(map.containsKey(key - a[i])){
+        return new int[]{a[i], (key - a[i])};
+      }else map.put(a[i], a[i]);
+    }
+    return null;
+  }
+}
